9e87c46e7e20b3d7686567110d78016de5a7a368
Clement Rodallec <crodallec@juniorisep.com>
2023-10-26 14:39:35 +0200

    fix : adapt modules from js to ts

--- modules/contractAddress.ts
+++ modules/contractAddress.ts
@@ -1,69 +1,61 @@
-const ethers = require("ethers");
-import { ZuploContext, ZuploRequest } from "@zuplo/runtime";
+import { ethers } from "ethers";
+import { ZuploContext, ZuploRequest, environment } from "@zuplo/runtime";
 
-const { WALLET_PRIVATE_KEY, QUICKNODE_API_KEY } = process.env;
+const QUICKNODE_API_KEY = environment.QUICKNODE_API_KEY;
 
 const RPCurl = 'https://attentive-convincing-pallet.matic-testnet.quiknode.pro/' + QUICKNODE_API_KEY + '/';
+const provider = new ethers.JsonRpcProvider(RPCurl);
 
 export default async function (request: ZuploRequest, context: ZuploContext) {
-  const txhash = await req.body.txhash;
+  const txhash = await request.json().txhash;
   if (typeof txhash != "string" || txhash.length != 66 || txhash.slice(0, 2) != "0x") {
-    res.send({
+    return {
       error: "Invalid argument type or format of txhash",
-    });
-    return res.end();
+    };
   } else {
     for (let i = 2; i < 66; i++) {
       if (txhash[i] < "0" || txhash[i] > "9") {
         if (txhash[i] < "A" || txhash[i] > "F") {
           if (txhash[i] < "a" || txhash[i] > "f") {
-            res.send({
+            return {
               error: "Invalid character at index " + i + " of txhash (current character at index " + i + " : \'" + txhash[i] + "\')",
-            });
-            return res.end();
+            };
           }
         }
       }
     }
   }
 
-  const provider = new ethers.JsonRpcProvider(RPCurl);
-
   try {
     const tx = await provider.getTransaction(txhash);
     if (tx == null) {
-      res.send({
+      return {
         error: "Transaction not found or not mined yet",
-      });
-      return res.end();
+      };
     }
   } catch (error) {
-    res.send({
+    return {
       error: "Invalid txhash",
       errorMessage: error.message,
-    });
-    return res.end();
+    };
   }
 
   try {
     const receipt = await provider.getTransactionReceipt(txhash);
 
     if (receipt.contractAddress) {
-      res.send({
+      return {
         contractAddress: receipt.contractAddress,
-      });
-      return res.end();
+      };
     } else {
-      res.send({
+      return {
         error: "No contract address found for this transaction, please verify that the transaction corresponds to a contract deployment",
-      });
-      return res.end();
+      };
     }
   } catch (error) {
-    res.send({
+    return {
       error: "Transaction not found or not mined yet",
       errorMessage: error.message,
-    });
-    return res.end();
+    };
   }
-});
+};
